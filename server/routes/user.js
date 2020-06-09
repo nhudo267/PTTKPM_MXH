7e97c363a74bebc53cbdc46357d36e48b7a04f2e
minh1710 <59530771+tienminh0509@users.noreply.github.com>
2020-06-09 10:35:36 +0700

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -23,27 +23,27 @@ router.get('/user/:id',requireLogin,(req,res)=>{
     })
 })
 
-// router.put('/follow',requireLogin,(req,res)=>{
-//     User.findByIdAndUpdate(req.body.followId,{
-//         $push:{followers:req.user._id}
-//     },{
-//         new:true
-//     },(err,result)=>{
-//         if(err){
-//             return res.status(422).json({error:err})
-//         }
-//       User.findByIdAndUpdate(req.user._id,{
-//           $push:{following:req.body.followId}
+router.put('/follow',requireLogin,(req,res)=>{
+    User.findByIdAndUpdate(req.body.followId,{
+        $push:{followers:req.user._id}
+    },{
+        new:true
+    },(err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }
+      User.findByIdAndUpdate(req.user._id,{
+          $push:{following:req.body.followId}
           
-//       },{new:true}).select("-password").then(result=>{
-//           res.json(result)
-//       }).catch(err=>{
-//           return res.status(422).json({error:err})
-//       })
+      },{new:true}).select("-password").then(result=>{
+          res.json(result)
+      }).catch(err=>{
+          return res.status(422).json({error:err})
+      })
 
-//     }
-//     )
-// })
+    }
+    )
+})//fix
 
 // router.put('/unfollow',requireLogin,(req,res)=>{
 //     User.findByIdAndUpdate(req.body.unfollowId,{
@@ -65,7 +65,7 @@ router.get('/user/:id',requireLogin,(req,res)=>{
 
 //     }
 //     )
-// })
+// }) test
 router.put('/updatepic',requireLogin,(req,res)=>{
     User.findByIdAndUpdate(req.user._id,{$set:{pic:req.body.pic}},{new:true},
         (err,result)=>{
